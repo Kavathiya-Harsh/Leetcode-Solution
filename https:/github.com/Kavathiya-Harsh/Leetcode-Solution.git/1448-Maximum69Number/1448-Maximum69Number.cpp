2bdc4f6892bd4d1ba5b97ab806447b2c6270bc29
HARSH KAVATHIYA <harsh.kavathiya.cg@gmail.com>
2026-06-02 09:45:59 +0530

    Create 1448-Maximum69Number

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1448-Maximum69Number/1448-Maximum69Number.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/2/2026, 9:45:58 AM
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
