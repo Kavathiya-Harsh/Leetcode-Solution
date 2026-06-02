01c03b5a787ee7006554d3ede146abad7caf0fed
HARSH KAVATHIYA <harsh.kavathiya.cg@gmail.com>
2026-06-02 09:45:45 +0530

    Create 2608-CountTheDigitsThatDivideANumber

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/2608-CountTheDigitsThatDivideANumber/2608-CountTheDigitsThatDivideANumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/2/2026, 9:45:44 AM
+class Solution {
+public:
+    int countDigits(int num) {
+        int temp = num;
+        int count = 0;
+
+        while(temp > 0){
+          int digit = temp % 10 ;
+
+          if (num % digit == 0){
+            count++;
+          }
+          temp = temp /10;
+        }
+        return count;
+    }
+};
