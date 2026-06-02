8252217e935f9576d80e98f204092c4a12d79e0e
HARSH KAVATHIYA <harsh.kavathiya.cg@gmail.com>
2026-06-02 09:46:31 +0530

    Create 78-Subsets

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/78-Subsets/78-Subsets.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/2/2026, 9:46:30 AM
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<vector<int>>ans;
+      vector<int> current;
+
+      solve(0,nums,current,ans);
+      return ans;
+    }
+
+    void solve(int index, vector<int>& nums,vector<int>& current,vector<vector<int>>&ans){
+      ans.push_back(current);
+
+      for(int i = index;i< nums.size();i++){
+        current.push_back(nums[i]);
+
+        solve (i + 1,nums,current,ans);
+
+        current.pop_back();
+      }
+    }
+};
