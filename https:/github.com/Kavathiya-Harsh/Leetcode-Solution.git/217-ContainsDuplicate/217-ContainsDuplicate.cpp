16544a7297b4dae0a96318013176e1f8eb0e7bf9
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-06-04 10:46:06 +0530

    File updated at 6/4/2026, 10:46:06 AM

--- https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,15 +1,16 @@
-// Last updated: 6/4/2026, 10:32:32 AM
+// Last updated: 6/4/2026, 10:46:06 AM
 1class Solution {
 2public:
-3    bool containsDuplicate(vector<int>& nums) {
-4        unordered_set<int> st;
+3    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+4        unordered_map<int, int> mp;
 5
-6        for (int num : nums) {
-7            if (st.count(num))
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (mp.count(nums[i]) && i - mp[nums[i]] <= k)
 8                return true;
-9            st.insert(num);
-10        }
-11
-12        return false;
-13    }
-14};
+9
+10            mp[nums[i]] = i;
+11        }
+12
+13        return false;
+14    }
+15};
