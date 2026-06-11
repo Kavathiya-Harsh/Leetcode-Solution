26db9ac5cb3c3bac64539783d095ca534afec843
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-06-11 08:12:25 +0530

    Create 367-ValidPerfectSquare

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/367-ValidPerfectSquare/367-ValidPerfectSquare.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/11/2026, 8:12:23 AM
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long left = 1, right = num;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            }
+            else if (square < num) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
