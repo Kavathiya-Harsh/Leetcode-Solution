3e21db58c8f693f785b5e6df96692214152b74eb
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-06-15 09:22:36 +0530

    Create 2128-ReversePrefixOfWord

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.cpp
@@ -0,0 +1,14 @@
+// Last updated: 6/15/2026, 9:22:35 AM
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int idx = word.find(ch);
+
+        if (idx == string::npos)
+            return word;
+
+        reverse(word.begin(), word.begin() + idx + 1);
+
+        return word;
+    }
+};
