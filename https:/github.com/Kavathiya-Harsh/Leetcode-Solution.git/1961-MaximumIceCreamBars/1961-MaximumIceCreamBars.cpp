da6cbab6782f17136e59cdfa32d50094b8149016
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-06-29 08:46:19 +0530

    Create 1961-MaximumIceCreamBars

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1961-MaximumIceCreamBars/1961-MaximumIceCreamBars.cpp
@@ -0,0 +1,28 @@
+// Last updated: 6/29/2026, 8:46:19 AM
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int maxCost = *max_element(costs.begin(), costs.end());
+
+        vector<int> freq(maxCost + 1, 0);
+
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+
+        int bars = 0;
+
+        for (int cost = 1; cost <= maxCost; cost++) {
+            if (freq[cost] == 0) continue;
+
+            int canBuy = min(freq[cost], coins / cost);
+
+            bars += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) break;
+        }
+
+        return bars;
+    }
+};
