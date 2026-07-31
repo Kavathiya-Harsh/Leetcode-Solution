a6bd65b891296af65094273812fe71e89e4a9d3f
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-07-31 13:27:12 +0530

    Create 1567-MaximumNumberOfVowelsInASubstringOfGivenLength

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1567-MaximumNumberOfVowelsInASubstringOfGivenLength/1567-MaximumNumberOfVowelsInASubstringOfGivenLength.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/31/2026, 1:27:14 PM
+class Solution {
+public:
+    bool isvowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    int maxVowels(string s, int k) {
+        int count = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (isvowel(s[i])) count++;
+        }
+
+        int ans = count;
+
+        for (int i = k; i < s.size(); i++) {
+            if (isvowel(s[i])) count++;
+            if (isvowel(s[i - k])) count--;
+
+            ans = max(ans, count);
+        }
+
+        return ans;
+    }
+};
