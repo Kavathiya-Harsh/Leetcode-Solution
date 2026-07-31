9f38d352730ddd80dd1c778c79701c5a02b045df
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-07-31 13:27:37 +0530

    Create 1222-RemoveCoveredIntervals

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1222-RemoveCoveredIntervals/1222-RemoveCoveredIntervals.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/31/2026, 1:27:38 PM
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+        // sort: start ↑ , end ↓
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        int count = 0;
+        int maxEnd = 0;
+
+        for (auto &it : intervals) {
+            if (it[1] > maxEnd) {
+                count++;           // not covered
+                maxEnd = it[1];
+            }
+        }
+
+        return count;
+    }
+};
