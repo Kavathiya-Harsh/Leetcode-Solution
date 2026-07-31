06dcc4c5fca7c8780d335da6c586ef442184bc93
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-07-31 13:27:45 +0530

    Create 1046-MaxConsecutiveOnesIii

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1046-MaxConsecutiveOnesIii/1046-MaxConsecutiveOnesIii.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/31/2026, 1:27:46 PM
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        int left = 0;
+        int zero = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            if (nums[right] == 0)
+                zero++;
+
+            while (zero > k) {
+
+                if (nums[left] == 0)
+                    zero--;
+
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
