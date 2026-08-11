4a1facef96692249834def066ddfbf327abca3c2
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-08-11 14:11:01 +0530

    Create 3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum/3236-SmallestMissingIntegerGreaterThanSequentialPrefixSum.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/11/2026, 2:11:01 PM
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0];
+
+        // Find longest sequential prefix
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                sum += nums[i];
+            else
+                break;
+        }
+
+        // Find smallest missing number >= sum
+        while (find(nums.begin(), nums.end(), sum) != nums.end())
+            sum++;
+
+        return sum;
+    }
+};
