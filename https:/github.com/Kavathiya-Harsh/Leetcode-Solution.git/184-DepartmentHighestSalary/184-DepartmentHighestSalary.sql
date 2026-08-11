19a886ea5226a785c9614ba53a61abe4b7c22d02
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-08-11 14:12:50 +0530

    Create 184-DepartmentHighestSalary

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,14 @@
+-- Last updated: 8/11/2026, 2:12:50 PM
+# Write your MySQL query statement below
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d 
+    ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
