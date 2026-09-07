dd82859fd14ee0c958af840b4b76e5aae30e91b8
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:40:04 +0530

    Create 2704-ToBeOrNotToBe

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/2704-ToBeOrNotToBe/2704-ToBeOrNotToBe.js
@@ -0,0 +1,31 @@
+// Last updated: 9/7/2026, 1:40:09 PM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+
+    return {
+
+        toBe: function(other) {
+            if (val === other) {
+                return true;
+            }
+
+            throw new Error("Not Equal");
+        },
+
+        notToBe: function(other) {
+            if (val !== other) {
+                return true;
+            }
+
+            throw new Error("Equal");
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
