188d014f2f3b39e268b816d3b43ad022ebd260e9
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:39:55 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/7/2026, 1:40:00 PM
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
