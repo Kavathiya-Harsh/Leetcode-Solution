3ab92e82a10a6d79c36b08a5aae4a751b0448209
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:41:30 +0530

    Create 1846-MaximumElementAfterDecreasingAndRearranging

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1846-MaximumElementAfterDecreasingAndRearranging/1846-MaximumElementAfterDecreasingAndRearranging.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/7/2026, 1:41:36 PM
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int ans = 1;
+
+        for (int i = 1; i < arr.size(); i++) {
+            ans = min(arr[i], ans + 1);
+        }
+
+        return ans;
+    }
+};
