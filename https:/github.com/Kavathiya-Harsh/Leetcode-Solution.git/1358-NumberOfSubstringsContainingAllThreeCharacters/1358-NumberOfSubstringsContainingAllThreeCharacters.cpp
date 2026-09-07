cb5a44a7de7014b1a3ff95528ac4d0c371b4abaa
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:42:32 +0530

    Create 1358-NumberOfSubstringsContainingAllThreeCharacters

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/1358-NumberOfSubstringsContainingAllThreeCharacters/1358-NumberOfSubstringsContainingAllThreeCharacters.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/7/2026, 1:42:37 PM
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> count(3, 0); // a, b, c
+        
+        int left = 0;
+        int result = 0;
+        
+        for (int right = 0; right < n; right++) {
+            count[s[right] - 'a']++;
+            
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                result += (n - right);
+                count[s[left] - 'a']--;
+                left++;
+            }
+        }
+        
+        return result;
+    }
+};
