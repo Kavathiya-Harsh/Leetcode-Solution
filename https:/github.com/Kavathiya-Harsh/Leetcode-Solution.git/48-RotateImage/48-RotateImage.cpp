361c823ef7316840bb1d068f3c55e557d642b0e2
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:45:05 +0530

    Create 48-RotateImage

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/48-RotateImage/48-RotateImage.cpp
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 1:45:10 PM
+class Solution {
+public:
+    void rotate(vector<vector<int>>& m) {
+        int n = m.size();
+
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
+                swap(m[i][j],m[j][i]);
+
+        for(auto &row:m)
+            reverse(row.begin(),row.end());
+    }
+};
