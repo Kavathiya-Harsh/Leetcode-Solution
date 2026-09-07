223f927199db30ac6733806f59fa4a39bbd1d01b
Harsh Kavathiya <harsh.kavathiya.cg@gmail.com>
2026-09-07 13:39:52 +0530

    Create 2958-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ https:/github.com/Kavathiya-Harsh/Leetcode-Solution.git/2958-LengthOfLongestSubarrayWithAtMostKFrequency/2958-LengthOfLongestSubarrayWithAtMostKFrequency.cpp
@@ -0,0 +1,19 @@
+// Last updated: 9/7/2026, 1:39:57 PM
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int l = 0, ans = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            mp[nums[r]]++;
+
+            while (mp[nums[r]] > k)
+                mp[nums[l++]]--;
+
+            ans = max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+};
